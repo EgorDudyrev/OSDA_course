d5f34d2a21fa8ae6c90ee258e4ebd53af56ba54a
Egor Dudyrev <egor.dudyrev@yandex.ru>
2022-09-27 21:15:09 +0300

    Refactor the example code

--- lazy_fca/lazy_pipeline.py
+++ lazy_fca/lazy_pipeline.py
@@ -1,58 +1,151 @@
-import copy
-from typing import Iterator
+import time
+from typing import Iterator, List, Collection, Callable
 
 import pandas as pd
 from tqdm import tqdm
 
+# Generalized prediction function type hint.
+# A prediction function takes a triplet of (test object description, train descriptions, train labels)
+# and outputs a bool prediction
+PREDICTION_FUNCTION_HINT = Callable[
+    [Collection[bool], Collection[Collection[bool]], Collection[bool]], bool
+]
 
-def load_data() -> pd.DataFrame:
-    df_test = pd.read_csv('lazy_example/data/test1.csv', sep=',')
-    df_train = pd.read_csv('lazy_example/data/train1.csv', sep=',')
-    df = pd.concat([df_train, df_test]).reset_index(drop=True)
 
-    df = df.replace(to_replace='positive', value=True)
-    df = df.replace(to_replace='negative', value=False)
+def load_data(df_name: str) -> pd.DataFrame:
+    """Generalized function to load datasets in the form of pandas.DataFrame"""
+    if df_name == 'tic_tac_toe':
+        return load_tic_tac_toe()
+
+    raise ValueError(f'Unknown dataset name: {df_name}')
+
+
+def load_tic_tac_toe() -> pd.DataFrame:
+    """Load tic-tac-toe dataset from UCI repository"""
+    column_names = [
+        'top-left-square', 'top-middle-square', 'top-right-square',
+        'middle-left-square', 'middle-middle-square', 'middle-right-square',
+        'bottom-left-square', 'bottom-middle-square', 'bottom-right-square',
+        'Class'
+    ]
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data'
+    df = pd.read_csv(url, names=column_names)
+    df['Class'] = [x == 'positive' for x in df['Class']]
     return df
 
 
-def binarize_data(X: pd.DataFrame) -> 'pd.DataFrame[bool]':
-    result_data = copy.deepcopy(X)
-    for column in X.columns.values:
-        result_data = pd.concat([
-            result_data,
-            pd.get_dummies(result_data[column], prefix=column, prefix_sep=': ')],
-            axis=1
-        )
-        del result_data[column]
-    return result_data.astype(bool)
+def binarize_X(X: pd.DataFrame) -> 'pd.DataFrame[bool]':
+    """Scale values from X into pandas.DataFrame of binary values"""
+    dummies = [pd.get_dummies(X[f], prefix=f, prefix_sep=': ') for f in X.columns]
+    X_bin = pd.concat(dummies, axis=1).astype(bool)
+    return X_bin
 
 
-def predict(x: 'pd.Series[bool]', X_train: 'pd.DataFrame[bool]', Y_train: 'pd.Series[bool]') -> bool:
-    iterator = ((x_train, y_train) for (_, x_train), y_train in zip(X_train.iterrows(), Y_train))
+def predict_with_generators(
+        x: List[bool], X_train: List[List[bool]], Y_train: List[bool],
+        min_cardinality: int = 1
+) -> bool:
+    """Lazy prediction for ``x`` based on training data ``X_train`` and ``Y_train``
 
-    pos, neg = 0, 0
-    for x_train, y_train in iterator:
-        if all(x & x_train == x_train):
-            if y_train:
-                pos += 1
-            else:
-                neg += 1
+    Parameters
+    ----------
+    x : List[bool]
+        Description to make prediction for
+    X_train: List[List[bool]]
+        List of training examples
+    Y_train: List[bool]
+        List of labels of training examples
+    min_cardinality: int
+        Minimal size of an intersection required to count for counterexamples
 
-    pos /= Y_train.sum()
-    neg /= (~Y_train).sum()
+    Returns
+    -------
+    prediction: bool
+        Class prediction for ``x`` (either True or False)
+    """
+    X_pos = [x_train for x_train, y in zip(X_train, Y_train) if y]
+    X_neg = [x_train for x_train, y in zip(X_train, Y_train) if not y]
 
-    return pos > neg
+    n_counters_pos = 0  # number of counter examples for positive intersections
+    for x_pos in X_pos:
+        intersection_pos = [(a and b) for a, b in zip(x, x_pos)]
+        if sum(intersection_pos) < min_cardinality:  # the intersection is too small
+            continue
+
+        for x_neg in X_neg:  # count all negative examples that contain intersection_pos
+            if all([a <= b for a, b in zip(intersection_pos, x_neg)]):
+                n_counters_pos += 1
+
+    n_counters_neg = 0  # number of counter examples for negative intersections
+    for x_neg in X_neg:
+        intersection_neg = [(a and b) for a, b in zip(x, x_neg)]
+        if sum(intersection_neg) < min_cardinality:
+            continue
+
+        for x_pos in X_pos:  # count all positive examples that contain intersection_neg
+            if all([a <= b for a, b in zip(intersection_neg, x_pos)]):
+                n_counters_neg += 1
+
+    perc_counters_pos = n_counters_pos / len(X_pos)
+    perc_counters_neg = n_counters_neg / len(X_neg)
+
+    prediction = perc_counters_pos < perc_counters_neg
+    return prediction
 
 
 def predict_array(
-        X: 'pd.DataFrame[bool]', Y: 'pd.Series[bool]',
-        n_train: int, update_train: bool = True, use_tqdm: bool = False
+        X: List[List[bool]], Y: List[bool],
+        n_train: int, update_train: bool = True, use_tqdm: bool = False,
+        predict_func: PREDICTION_FUNCTION_HINT = predict_with_generators
 ) -> Iterator[bool]:
-    for i, (_, x) in tqdm(
-        enumerate(X[n_train:].iterrows()),
+    """Predict the labels of multiple examples from ``X``
+
+    Parameters
+    ----------
+    X: List[List[bool]]
+        Set of train and test examples to classify
+    Y: List[bool]
+        Set of train and test labels for each example from X
+    n_train: int
+        Initial number of train examples. That is, make predictions only for examples from X_train[n_train:]
+    update_train: bool
+        A flag whether to consider true labels of predicted examples as training data or not.
+        If True, then for each X_i the training data consists of X_1, X_2, ..., X_{n_train}, ...,  X_{i-1}.
+        If False, then for each X_i the training data consists of X_1, X_2, ..., X_{n_train}
+    use_tqdm: bool
+        A flag whether to use tqdm progress bar (in case you like progress bars)
+    predict_func: <see PREDICTION_FUNCTION_HINT defined in this file>
+        A function to make prediction for each specific example from ``X``.
+        The default prediction function is ``predict_with_generator`` (considered as baseline for the home work).
+
+    Returns
+    -------
+    prediction: Iterator
+        Python generator with predictions for each x in X[n_train:]
+    """
+    for i, x in tqdm(
+        enumerate(X[n_train:]),
         initial=n_train, total=len(X),
         desc='Predicting step by step',
         disable=not use_tqdm,
     ):
         n_trains = n_train + i if update_train else n_train
-        yield predict(x, X[:n_trains], Y[:n_trains])
+        yield predict_func(x, X[:n_trains], Y[:n_trains])
+
+
+def apply_stopwatch(iterator: Iterator):
+    """Measure run time of each iteration of ``iterator``
+
+    The function can be applied e.g. for the output of ``predict_array`` function
+    """
+    outputs = []
+    times = []
+
+    t_start = time.time()
+    for out in iterator:
+        dt = time.time() - t_start
+        outputs.append(out)
+        times.append(dt)
+        t_start = time.time()
+
+    return outputs, times
